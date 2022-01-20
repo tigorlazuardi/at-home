559aa369b12057e7e603fcb84a452676b85d99ac
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-01-20 14:11:15 +0700

    feat(sitemap): added next sitemap and robot txt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	eslint: {
+		dirs: ['src'],
+	},
+	images: {
+		domains: ['tigor.web.id'],
+	},
 }
 
 module.exports = nextConfig
